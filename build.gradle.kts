a05e98f7f6fec73e759d410a986935e5ff1af392
Chrisliebär <mail@chrisliebaer.de>
2021-01-04 11:29:35 +0100

    Initial commit with hello world application

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.tasks.compile.JavaCompile
+
+
+plugins {
+	kotlin("jvm") version "1.4.30-M1"
+	id("io.freefair.lombok") version "5.3.0"
+	idea
+	application
+	java
+}
+apply {
+	plugin("kotlin")
+}
+
+repositories {
+	mavenLocal()
+	mavenCentral()
+	jcenter()
+}
+
+application {
+	mainClass.set("de.chrisliebaer.HelloWorld")
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_15
+	targetCompatibility = JavaVersion.VERSION_15
+}
+
+idea {
+	module {
+		isDownloadJavadoc = true
+		isDownloadSources = true
+	}
+}
+
+tasks.withType(KotlinCompile::class.java).all {
+	kotlinOptions {
+		jvmTarget = "15"
+	}
+}
+
+tasks.withType(JavaCompile::class.java).all {
+	options.encoding = "UTF-8"
+}
---
+++ src/main/kotlin/de/chrisliebaer/helloworld/HelloWorld.kt
@@ -0,0 +1,5 @@
+package de.chrisliebaer.helloworld
+
+fun main() {
+	print("Hello World, this is me!")
+}
